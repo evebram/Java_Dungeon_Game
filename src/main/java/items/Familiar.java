032291b0bfec43957baab7ae2dae4a9b0d112602
= <evebramley@me.com>
2019-09-13 11:28:39 +0100

    familiar enum

--- src/main/java/items/Familiar.java
+++ src/main/java/items/Familiar.java
@@ -1,4 +1,18 @@
 package items;
 
 public enum Familiar {
+
+    TOAD(2),
+    OWL(3),
+    WYVERN(4);
+
+    private final int resistance;
+
+    Familiar(int resisitance) {
+        this.resistance = resisitance;
+    }
+
+    public int getResistance() {
+        return resistance;
+    }
 }
