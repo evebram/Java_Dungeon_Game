d8dbfb7761939c93ec7bcb85ae3eecae76563d42
= <evebramley@me.com>
2019-09-13 11:32:38 +0100

    healingitem enum

--- src/main/java/items/HealingItem.java
+++ src/main/java/items/HealingItem.java
@@ -1,4 +1,17 @@
 package items;
 
 public enum HealingItem {
+    HERBS(5),
+    POTION(10),
+    PRAYEROFHEALING(15);
+
+    private final int healing;
+
+    HealingItem(int healing) {
+        this.healing = healing;
+    }
+
+    public int getHealing() {
+        return this.healing;
+    }
 }
